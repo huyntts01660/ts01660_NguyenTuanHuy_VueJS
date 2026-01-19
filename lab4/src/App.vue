849d8783601fdd95bf02e76cf160564528bcb5e2
huyntts01660 <huyntts01660@gmail.com>
2026-01-20 04:32:58 +0700

    lab4

---
+++ lab4/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Bai4 from './components/Bai4.vue'
+// import Bai3 from './components/Bai3.vue'
+</script>
+
+<template>
+  <!-- <div>
+    <h1>Chào mừng đến với VueJS</h1>
+    <Bai3 />
+  </div> -->
+  <Bai4 />
+</template>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
---
+++ lab4/src/components/Bai3.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1>{{ greeting }}</h1>
+    <button
+        :title="buttonTitle"
+        @click="changeGreeting">
+        click de thay doi
+    </button>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            greeting: 'Xin chào bạn!',
+            buttonTitle: 'Nhấn vào đây để thay đổi lời chào'
+        }
+    },
+    methods: {
+        changeGreeting() {
+            this.greeting = 'hello, world!'
+            this.buttonTitle = 'Lời chào mới đã được cập nhật'
+        }
+    }
+}
+</script>
